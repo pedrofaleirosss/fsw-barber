88047590251b558911f8ee6b3f19605257ec1e14
Pedro Faleiros <pedro.alves.faleiros@gmail.com>
2024-08-06 21:16:15 -0300

    feat: add BookingItem component

---
+++ app/_components/booking-item.tsx
@@ -0,0 +1,33 @@
+import { Avatar, AvatarImage } from "./ui/avatar"
+import { Badge } from "./ui/badge"
+import { Card, CardContent } from "./ui/card"
+
+const BookingItem = () => {
+  return (
+    <>
+      <Card>
+        <CardContent className="flex justify-between px-5 py-0">
+          <div className="flex flex-col gap-2 py-5">
+            <Badge className="w-fit">Confirmado</Badge>
+            <h3 className="font-semibold">Corte de Cabelo</h3>
+
+            <div className="flex items-center gap-2">
+              <Avatar className="h-6 w-6">
+                <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png" />
+              </Avatar>
+              <p className="text-sm">Barbearia FSW</p>
+            </div>
+          </div>
+
+          <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5 pl-10">
+            <p className="text-sm">Agosto</p>
+            <p className="text-2xl">06</p>
+            <p className="text-sm">19:00</p>
+          </div>
+        </CardContent>
+      </Card>
+    </>
+  )
+}
+
+export default BookingItem
