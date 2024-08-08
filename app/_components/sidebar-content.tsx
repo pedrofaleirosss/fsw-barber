a2611ba30d994422267467a30ba56cef50f3b621
Pedro Faleiros <pedro.alves.faleiros@gmail.com>
2024-08-08 20:04:41 -0300

    chore: add next auth setup

--- app/_components/sidebar-content.tsx
+++ app/_components/sidebar-content.tsx
@@ -1,5 +1,5 @@
 import { Button } from "./ui/button"
-import { CalendarIcon, HomeIcon, LogOutIcon } from "lucide-react"
+import { CalendarIcon, HomeIcon, LogInIcon, LogOutIcon } from "lucide-react"
 import {
   SheetContent,
   SheetDescription,
@@ -10,6 +10,14 @@ import { quickSearchOptions } from "../_constants/search"
 import { Avatar, AvatarImage } from "./ui/avatar"
 import Link from "next/link"
 import Image from "next/image"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog"
 
 const SidebarContent = () => {
   return (
@@ -20,15 +28,40 @@ const SidebarContent = () => {
 
       <SheetDescription></SheetDescription>
 
-      <div className="flex items-center gap-3 border-b border-solid py-5">
-        <Avatar>
+      <div className="flex items-center justify-between gap-3 border-b border-solid py-5">
+        {/* <Avatar>
           <AvatarImage src="https://github.com/pedrofaleirosss.png" />
         </Avatar>
 
         <div>
           <p className="font-bold">Pedro Faleiros</p>
           <p className="text-xs">pedro.alves.faleiros@gmail.com</p>
-        </div>
+        </div> */}
+        <h2 className="font-bold">Olá, faça seu login!</h2>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button size="icon">
+              <LogInIcon />
+            </Button>
+          </DialogTrigger>
+          <DialogContent className="w-[90%] rounded-xl">
+            <DialogHeader>
+              <DialogTitle>Faça seu login na plataforma</DialogTitle>
+              <DialogDescription>
+                Conecte-se usando sua conta do Google
+              </DialogDescription>
+            </DialogHeader>
+            <Button variant="outline" className="gap-1 font-bold">
+              <Image
+                src="/google.svg"
+                alt="Fazer login com o Google"
+                width={18}
+                height={18}
+              />
+              Google
+            </Button>
+          </DialogContent>
+        </Dialog>
       </div>
 
       <div className="flex flex-col gap-2 border-b border-solid py-5">
