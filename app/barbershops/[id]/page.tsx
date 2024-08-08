584491a7a5ade286a0a70145adaaa59b12c0e9f0
Pedro Faleiros <pedro.alves.faleiros@gmail.com>
2024-08-07 21:05:54 -0300

    feat: add PhoneItem component and Footer to layoult

--- app/barbershops/[id]/page.tsx
+++ app/barbershops/[id]/page.tsx
@@ -1,3 +1,4 @@
+import PhoneItem from "@/app/_components/phone-item"
 import ServiceItem from "@/app/_components/service-item"
 import { Button } from "@/app/_components/ui/button"
 import { db } from "@/app/_lib/prisma"
@@ -76,10 +77,19 @@ const BarbershopPage = async ({ params }: BarbershopPageProps) => {
         <p className="text-justify text-sm">{barbershop.description}</p>
       </div>
 
-      <div className="space-y-3 p-5">
-        <h2 className="text-xs font-bold uppercase text-gray-400">Serviços</h2>
+      <div className="space-y-3 border-b border-solid p-5">
+        <h2 className="mb-5 text-xs font-bold uppercase text-gray-400">
+          Serviços
+        </h2>
         {barbershop.services.map((service) => (
           <ServiceItem key={service.id} service={service} />
+        ))}
+      </div>
+
+      <div className="space-y-3 p-5">
+        <h2 className="text-xs font-bold uppercase text-gray-400">Contato</h2>
+        {barbershop.phones.map((phone, i) => (
+          <PhoneItem phone={phone} key={i} />
         ))}
       </div>
     </div>
