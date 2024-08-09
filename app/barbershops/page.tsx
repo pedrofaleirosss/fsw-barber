05f68288b77356f98be73b108cade9e0f18f06f2
Pedro Faleiros <pedro.alves.faleiros@gmail.com>
2024-08-08 21:50:16 -0300

    feat: ensure user can search for barbershops

---
+++ app/barbershops/page.tsx
@@ -0,0 +1,45 @@
+import BarbershopItem from "../_components/barbershop-item"
+import Header from "../_components/header"
+import Search from "../_components/search"
+import { db } from "../_lib/prisma"
+
+interface BarbershopsPageProps {
+  searchParams: {
+    search?: string
+  }
+}
+
+const BarbershopsPage = async ({ searchParams }: BarbershopsPageProps) => {
+  const barbershops = await db.barbershop.findMany({
+    where: {
+      name: {
+        contains: searchParams?.search,
+        mode: "insensitive",
+      },
+    },
+  })
+
+  return (
+    <div>
+      <Header />
+
+      <div className="px-5 pb-5">
+        <div className="mt-6">
+          <Search />
+        </div>
+
+        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+          Resultados para {`"${searchParams.search}"`}
+        </h2>
+
+        <div className="grid grid-cols-2 gap-4">
+          {barbershops.map((barbershop) => (
+            <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+          ))}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default BarbershopsPage
